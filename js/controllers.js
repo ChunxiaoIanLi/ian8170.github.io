9aad9c56a52e0ff767186a146a6607292f5fc669
ian8170 <xiao__1993@hotmail.com>
2015-02-03 02:22:01 -0500

    changing everything lol

--- js/controllers.js
+++ js/controllers.js
@@ -1,25 +1,13 @@
 var personalWebControllers = angular.module('personalWebControllers', []);
 
 personalWebControllers.controller("HomeCtrl", ["$scope", function ($scope) {
-	$scope.currentTab="me";
-	$scope.isActive=function(tab){
-		return $scope.currentTab==tab;
-	};
-	$scope.toggle=function(tab){
-		$scope.currentTab=tab;
-	};
+	
 }]);
 
-personalWebControllers.controller("MeCtrl", ["$scope",  function ($scope) {
-
-}]);
 
 personalWebControllers.controller("ProjectCtrl", ["$scope", function ($scope) {
 	$scope.projects=[{name:'GradeTacker', url:'project/GradeTacker/index.html', intro:"this is GradeTacker"},
 	{name:'AnxietyResearchApp', url:'project/AnxietyResearchApp/index.html', intro:"this is AnxietyResearchApp"}];
 }]);
 
-personalWebControllers.controller("ContactCtrl", ["$scope", function ($scope) {
-
-}]);
 
